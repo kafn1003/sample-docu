836ff4f17bae2efe1baa5db6a27ac1a488805c71
KYLE <kyleniosco@gmail.com>
2024-01-02 14:58:52 +0800

    Updated Config

--- docu-sample/docusaurus.config.js
+++ docu-sample/docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://kafn1003.github.io',
+  url: 'https://docu-sample.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sample-docu/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kafn1003', // Usually your GitHub org/user name.
+  organizationName: 'docu-sample', // Usually your GitHub org/user name.
   projectName: 'sample-docu', // Usually your repo name.
 
   onBrokenLinks: 'throw',
